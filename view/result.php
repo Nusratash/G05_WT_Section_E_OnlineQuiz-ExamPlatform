b8463d7a2ca6c6d36aede8aaa88de5ee29336796
Saib <mohammadsaib2003@gmail.com>
2026-05-13 05:35:31 +0600

    Created a demo, content will be replaced later

---
+++ view/result.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" style="background-color: black; color: white">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Result</h1>
+    <table>
+        <tr>
+            <td colspan="2">Question-1</td>
+        </tr>
+        <tr>
+            <td>Option-1</td>
+            <td>Option-2</td>
+        </tr>
+        <tr>
+            <td>Option-3</td>
+            <td>Option-4</td>
+        </tr>
+    </table>
+</body>
+</html>
