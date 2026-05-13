2ec97af440116819233b5ebe43ff42d96f4f81de
Saib <mohammadsaib2003@gmail.com>
2026-05-13 10:25:45 +0600

    Changed loction for DatabaseConnection.php to /model from /controller

--- view/result.php
+++ view/result.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table{
+            border-collapse: collapse;
+        }
+        td{
+            border: 1px solid white;
+        }
+    </style>
 </head>
 <body>
     <h1>Result</h1>
@@ -12,12 +20,14 @@
             <td colspan="2">Question-1</td>
         </tr>
         <tr>
-            <td>Option-1</td>
-            <td>Option-2</td>
+            <td>Score: [student_score]</td>
+            <td>Status: [pass/fail]</td>
         </tr>
         <tr>
-            <td>Option-3</td>
-            <td>Option-4</td>
+            <td colspan="2">Your Answer: [Answer given by student]</td>
+        </tr>
+        <tr>
+            <td colspan="2">Correct Answer: [Correct answer from db]</td>
         </tr>
     </table>
 </body>
