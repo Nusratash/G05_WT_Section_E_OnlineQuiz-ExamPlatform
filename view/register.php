5c45ea09851421d57c5b13fe445927421250a8fb
Hashamee Al Shahriar <122994109+HashameeAlShahriar@users.noreply.github.com>
2026-05-15 00:12:27 -0700

    add register

--- view/register.php
+++ view/register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 $nameError = $_SESSION["nameErr"] ?? "";
 $emailError = $_SESSION["emailErr"] ?? "";
@@ -25,6 +25,7 @@ unset($_SESSION["errorMsg"]);
 
 
 <html>
+
 <head>
     <title>Registration</title>
 
@@ -32,9 +33,85 @@ unset($_SESSION["errorMsg"]);
 
 <body>
 
+    <div class="container">
+        <h2>Register Dashboard</h2>
+
+        <?php
+        if ($successMsg) {
+            echo $successMsg;
+        } elseif ($errorMsg) {
+            echo $errorMsg;
+        }
+        ?>
+
+
+
+        <form method="post" action="../Controller/RegisterController.php">
+
+            <table>
+                <tr>
+                    <td><label>Full Name:</label></td>
+                    <td><input type="text" name="name" placeholder="Enter your full name" value="<?php echo $name; ?>" /></td>
+                    <td>
+                        <p style='color:red;'><?php echo $nameError; ?></p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><label>Email:</label></td>
+                    <td><input type="email" name="email" placeholder="Enter your email" value="<?php echo $email; ?>" /></td>
+                    <td>
+                        <p style='color:red;'><?php echo $emailError; ?></p>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td><label>Password:</label></td>
+                    <td><input type="password" name="password" placeholder="Enter password (>= 8 char)" /></td>
+                    <td>
+                        <p style='color:red;'><?php echo $passwordError; ?></p>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td><label>Role:</label></td>
+                    <td>
+                        <div class="radio-group">
+                            <label>
+                                <input type="radio" name="role" value="student" <?php echo $role == 'student' ? 'checked' : ''; ?> /> Student
+                            </label>
+                            <label>
+                                <input type="radio" name="role" value="instructor" <?php echo $role == 'instructor' ? 'checked' : ''; ?> /> Instructor
+                            </label>
+                        </div>
+                    </td>
+                    
+                    <td>
+                        <p style='color:red;'><?php echo $roleError; ?></p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><button type="submit" name="submit">Register</button></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td>Already have an account? <a href='login.php'>Login Here</a></td>
+                    <td></td>
+                </tr>
+            </table>
+
+        </form>
+    </div>
 
 
 
 
 </body>
+
 </html>
