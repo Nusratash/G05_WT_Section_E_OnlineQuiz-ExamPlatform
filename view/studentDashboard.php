2882755f419698c49ac2215767f804fee0b50492
Hashamee Al Shahriar <122994109+HashameeAlShahriar@users.noreply.github.com>
2026-05-15 01:16:48 -0700

    add s

--- view/studentDashboard.php
+++ view/studentDashboard.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 include "../Model/UserModel.php";
 
@@ -7,15 +7,15 @@ $role = $_SESSION["role"] ?? "";
 $name = $_SESSION["name"] ?? "";
 $userId = $_SESSION["user_id"] ?? "";
 
-if(!$isLoggedIn){
+if (!$isLoggedIn) {
     Header("Location: login.php");
     exit();
 }
 
-if($role != 'student'){
-    if($role == 'instructor'){
+if ($role != 'student') {
+    if ($role == 'instructor') {
         Header("Location: instructor_dashboard.php");
-    }elseif($role == 'admin'){
+    } elseif ($role == 'admin') {
         Header("Location: admin_panel.php");
     }
     exit();
@@ -27,39 +27,49 @@ $a = $userModel->getStudentDashboardStats($userId);
 
 <!doctype html>
 <html>
-    <head>
-        <title>Student Dashboard</title>
-         <div class="dashboard">
+
+<head>
+    <title>Student Dashboard</title>
+    <div class="dashboard">
+        <div>
             <div>
-                <div>
-                    <h1>Welcome, <?php echo $name; ?>!</h1>
-                    <h3>Student Dashboard</h3>
-                </div>
-                <a href="../Controller/LogoutController.php" class="logout">Logout</a>
+                <h1>Welcome, <?php echo $name; ?>!</h1>
+                <h3>Student Dashboard</h3>
             </div>
-            
-            <div>
-                <div>
-                    <h3>Available Quizzes :</h3>
-                    <div><?php echo $a['total_quizzes']; ?></div>
-                    
-                </div>
-                 <div>
-                    <h3>Attempts Taken of quizzes:</h3>
-                    <div><?php echo $a['total_attempts']; ?></div>
-                  
-                </div>
-                
-                
-                
-               
+            <a href="../Controller/LogoutController.php" class="logout">Logout</a>
         </div>
-    
-    </head>
-    <body>
-       
+
+        <div>
+            <div>
+                <h3>Available Quizzes :</h3>
+                <div><?php echo $a['total_quizzes']; ?></div>
+
+            </div>
+            <div>
+                <h3>Attempts Taken of quizzes:</h3>
+                <div><?php echo $a['total_attempts']; ?></div>
+
+            </div>
+
+            <div>
+                <h3>Total Score :</h3>
+                <div><?php echo $a['total_score']; ?></div>
+                
+            </div>
+        </div>
+
+        
+
+
+    </div>
+
+</head>
+
+<body>
 
 
 
-    </body>
+
+</body>
+
 </html>
