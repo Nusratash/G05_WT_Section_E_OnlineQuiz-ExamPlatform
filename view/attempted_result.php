b381de9212d72d4fc9666fb4dd87831047c74826
Saib <mohammadsaib2003@gmail.com>
2026-05-16 21:43:58 +0600

    added the table to show quiz result

--- view/attempted_result.php
+++ view/attempted_result.php
@@ -5,3 +5,45 @@ $quizTitle = $_SESSION["quizTitle"] ?? "";
 $quizTotalMark = $_SESSION["quizTotalMark"] ?? "";
 $quizScore = $_SESSION["quizScore"] ?? "";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Attempt Result</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td colspan="3" style="text-align: center"><h3><?php echo $quizTitle;?></h3></td>
+        </tr>
+        <tr>
+            <td class="cell-align-right"><b>Score:</b> <?php echo $quizScore."/".$quizTotalMark;?></td>
+            <td></td>
+            <td style="text-align: right"><b>Status:</b> <?php echo ($quizScore/$quizTotalMark>=.6 ? "Passed":"Failed") ?></td>
+        </tr>
+        <?php
+        foreach($resultData as $row)
+        {
+        ?>
+        <tr>
+            <td class="cell-align-right"><b><?php echo $row["order_index"].". "; ?></b></td>
+            <td colspan="2" class="max-width"><b><?php echo $row["question_text"]; ?></b></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td colspan="2" class="<?php echo($row['selected_answer']==$row['correct_answer'])?'correct':'wrong'?>"><b>Selected Option:</b> <?php echo $row["selected_answer"]; ?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td colspan="2"><b>Correct Option:</b> <?php echo $row["correct_answer"];?></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+
+</body>
+
+</html>
