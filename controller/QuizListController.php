23b72e904ec051628e14f43567193a5f909457b6
iamrisan <jahidulislamrisan20@gmail.com>
2026-05-17 15:29:01 +0600

    added controller folder

---
+++ controller/QuizListController.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+require_once("../model/DatabaseConnection.php");
+
+require_once("../model/DBQuizList.php");
+
+
+$db=new DatabaseConnection();
+$connection=$db->openConnection();
+
+
+$student_id=$_SESSION["student_id"];
+
+$obj=new DBQuizList();
+
+$result=$obj->BringQuizList($connection,$student_id);
+
+$_SESSION["quizList"]=[];
+
+while($row=$result->fetch_assoc())
+{
+    $_SESSION["quizList"][]=[
+        "id"=>$row["id"],
+        "title"=>$row["title"],
+        "total_marks"=>$row["total_marks"],
+        "score"=>$row["score"],
+        "attempt_id"=>$row["attempt_id"]
+    ];
+}
+
+
+header("Location: ../view/quiz_list.php");
+exit();
+
+?>
